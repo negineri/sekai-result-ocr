18753f7c8711a735c9f9f2359a58d6b97e7cef62
negineri <harusoin@gmail.com>
2021-05-10 02:46:36 +0900

    feat: file upload

--- main.py
+++ main.py
@@ -11,30 +11,48 @@ os.makedirs("tmp", exist_ok=True)
 
 app = Flask(__name__)
 
+UPLOAD_FOLDER = './tmp/'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
 
 @app.route('/')
 def hello():
-    if request.args.get('src') is None:
-        return render_template("home/index.html")
-    src_url = request.args.get('src')
-    try:
-        file_data = requests.get(src_url)
-    except requests.exceptions.RequestException as err:
-        print(err)
-        return '{message: "有効なURLではありません"}'
-    file_path = 'tmp/' + str(int(random.random() * 10000000000))
-    with open(file_path, mode='wb') as f:
-        f.write(file_data.content)
+    return render_template("home/index.html")
+
+
+@app.route('/ocr', methods=["GET", "POST"])
+def ocr():
+    file_path = ""
+    if request.method == "POST":
+        if request.files['src_file'].filename == '':
+            return '{status: "error", message: "ファイルを指定してください"}'
+        img_file = request.files['src_file']
+        file_path = os.path.join(app.config['UPLOAD_FOLDER'], str(int(random.random() * 10000000000)))
+        img_file.save(file_path)
+    else:
+        if request.args.get('src') is None:
+            return '{status: "error", message: "srcを指定してください"}'
+        src_url = request.args.get('src')
+        try:
+            file_data = requests.get(src_url)
+        except requests.exceptions.RequestException as err:
+            print(err)
+            return '{status: "error", message: "有効なURLではありません"}'
+        file_path = 'tmp/' + str(int(random.random() * 10000000000))
+        with open(file_path, mode='wb') as f:
+            f.write(file_data.content)
     ext = imghdr.what(file_path)
     if ext is None:
-        return '{message: "非対応の画像形式です"}'
+        return '{status: "error", message: "非対応の画像形式です"}'
     os.rename(file_path, file_path + "." + ext)
     file_path = file_path + "." + ext
     result = ocr_result.loadfile(file_path)
     if result is None:
-        return '{message: "スコアを取得出来ませんでした"}'
+        return '{status: "error", message: "スコアを取得出来ませんでした"}'
     os.remove(file_path)
-    return json.dumps(result.to_dict())
+    res = result.to_dict()
+    res["status"] = "ok"
+    return json.dumps(res)
 
 
 if __name__ == "__main__":
