28ff1fddf2e8afb93ea48d8495f0182fae17a043
negineri <harusoin@gmail.com>
2021-05-13 01:01:51 +0900

    feat: resize src image

--- ocr_result.py
+++ ocr_result.py
@@ -88,6 +88,13 @@ class ScoreResultChecker:
             self.music_combos[music_title][d["musicDifficulty"]] = d["noteCount"]
 
 
+def __resize_image(img, magnification):
+    ImgWidth = img.width * magnification
+    ImgHeight = img.height * magnification
+    img_resize = img.resize((int(ImgWidth), int(ImgHeight)), Image.LANCZOS)
+    return img_resize
+
+
 def loadfile(fp: str, debug=False) -> Optional[ScoreResult]:
     tools = pyocr.get_available_tools()
     if len(tools) == 0:
@@ -96,6 +103,7 @@ def loadfile(fp: str, debug=False) -> Optional[ScoreResult]:
     tool = tools[0]
     result = ScoreResult()
     im = Image.open(fp)
+    im = __resize_image(im, 1080 / im.height + 1)
     bg = Image.new("L", im.size)
     im_mono = im.convert("L").point(lambda _: 1 if _ > 10 else 0, mode="1")
     diff = ImageChops.difference(im_mono, bg)
@@ -115,7 +123,7 @@ def loadfile(fp: str, debug=False) -> Optional[ScoreResult]:
         w = int(im.height * 16 / 9)
         h = im.height
         im_crop = im.crop((int((im.width - w) / 2), 0, int((im.width - w) / 2) + w, h))
-        im_difficulty = im_crop.crop((int(w * 0.091), int(h * 0.077), int(w * 0.175), int(h * 0.117))).convert("L").point(lambda _: 0 if _ > 200 else 1, mode="1")
+        im_difficulty = im_crop.crop((int(w * 0.091), int(h * 0.077), int(w * 0.175), int(h * 0.117))).convert("L").point(lambda _: 0 if _ > 180 else 1, mode="1")
         im_title = im_crop.crop((int(w * 0.081), 0, int(w * 0.5), int(h * 0.066))).convert("L").point(lambda _: 0 if _ > 180 else 1, mode="1")
 
     im_score = im_crop.crop((int(w * 0.041), int(h * 0.622), int(w * 0.526), int(h * 0.964)))
@@ -139,13 +147,17 @@ def loadfile(fp: str, debug=False) -> Optional[ScoreResult]:
         result.live = "normal"
         if not miss.isdecimal():
             return None
-    result.miss = int(miss)
     if debug:
-        im_score.save("data/dst/score.png", "PNG")
+        print(f"miss: {miss} / ", end="")
+        im_miss.save("data/dst/miss.png", "PNG")
+    result.miss = int(miss)
 
     im_perfect = im_score.crop((int(im_score.width * 0.859), int(im_score.height * 0.249), int(im_score.width * 0.978), int(im_score.height * 0.389))).\
         convert('1', dither=Image.NONE).point(lambda _: 1 if _ == 0 else 0)
     perfect = tool.image_to_string(im_perfect, lang="eng", builder=pyocr.builders.TextBuilder(tesseract_layout=7))
+    if debug:
+        print(f"perfect: {perfect} / ", end="")
+        im_perfect.save("data/dst/perfect.png", "PNG")
     if not perfect.isdecimal():
         return None
     result.perfect = int(perfect)
@@ -153,6 +165,9 @@ def loadfile(fp: str, debug=False) -> Optional[ScoreResult]:
     im_great = im_score.crop((int(im_score.width * 0.859), int(im_score.height * 0.389), int(im_score.width * 0.978), int(im_score.height * 0.532))).\
         convert('1', dither=Image.NONE).point(lambda _: 1 if _ == 0 else 0)
     great = tool.image_to_string(im_great, lang="eng", builder=pyocr.builders.TextBuilder(tesseract_layout=7))
+    if debug:
+        print(f"great: {great} / ", end="")
+        im_great.save("data/dst/great.png", "PNG")
     if not great.isdecimal():
         return None
     result.great = int(great)
@@ -160,6 +175,9 @@ def loadfile(fp: str, debug=False) -> Optional[ScoreResult]:
     im_good = im_score.crop((int(im_score.width * 0.859), int(im_score.height * 0.532), int(im_score.width * 0.978), int(im_score.height * 0.675))).\
         convert('1', dither=Image.NONE).point(lambda _: 1 if _ == 0 else 0)
     good = tool.image_to_string(im_good, lang="eng", builder=pyocr.builders.TextBuilder(tesseract_layout=7))
+    if debug:
+        print(f"good: {good} / ", end="")
+        im_good.save("data/dst/good.png", "PNG")
     if not good.isdecimal():
         return None
     result.good = int(good)
@@ -167,16 +185,28 @@ def loadfile(fp: str, debug=False) -> Optional[ScoreResult]:
     im_bad = im_score.crop((int(im_score.width * 0.859), int(im_score.height * 0.675), int(im_score.width * 0.978), int(im_score.height * 0.818))).\
         convert('1', dither=Image.NONE).point(lambda _: 1 if _ == 0 else 0)
     bad = tool.image_to_string(im_bad, lang="eng", builder=pyocr.builders.TextBuilder(tesseract_layout=7))
+    if debug:
+        print(f"bad: {bad} / ", end="")
+        im_bad.save("data/dst/bad.png", "PNG")
     if not bad.isdecimal():
         return None
     result.bad = int(bad)
 
     title = tool.image_to_string(im_title, lang="jpn", builder=pyocr.builders.TextBuilder(tesseract_layout=7))
+    if debug:
+        print(f"title: {title} / ")
     result.title = title
 
     difficulty = tool.image_to_string(im_difficulty, lang="eng", builder=pyocr.builders.TextBuilder(tesseract_layout=7))
-    if difficulty not in ["EASY", "NORMAL", "HARD", "EXPERT", "MASTER"]:
-        return None
-    result.difficulty = difficulty
+    if debug:
+        print(f"difficulty: {difficulty}")
+    match_ratio = 0
+    correct_difficulty = ""
+    for d in ["EASY", "NORMAL", "HARD", "EXPERT", "MASTER"]:
+        s = difflib.SequenceMatcher(None, d, difficulty).ratio()
+        if s > match_ratio:
+            match_ratio = s
+            correct_difficulty = d
+    result.difficulty = correct_difficulty
 
     return result
